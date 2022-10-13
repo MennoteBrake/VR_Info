8f1f0b12fd7f80e1322057fee0f4e75a5d09b7a0
Erik <erik@MacBook-Pro-van-Erik.local>
2022-10-13 10:44:14 +0300

    VRI-33 Implemented saving setting in database

--- ios/VR_Info/AppDelegate.mm
+++ ios/VR_Info/AppDelegate.mm
@@ -6,6 +6,8 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import "RNSplashScreen.h"
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -57,6 +59,9 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  [RNSplashScreen show];
+
   return YES;
 }
 
