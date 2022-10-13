f6a905d085e52aed15030707b663bc40f13ea256
MennoteBrake <m.tebrake@teneo-iot.nl>
2022-10-13 11:19:46 +0300

    added launch screen for android

--- android/app/src/main/java/com/vr_info/MainApplication.java
+++ android/app/src/main/java/com/vr_info/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new SplashScreenReactPackage());
+          // packages.add(new SplashScreenReactPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
